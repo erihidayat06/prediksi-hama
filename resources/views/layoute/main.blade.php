ad6b5c6cf42a52a8a7682f4e95877078463a35ae
erihidayat06 <erihidayat549@gmail.com>
2025-02-24 21:24:07 +0700

    Menambah navbar

--- resources/views/layoute/main.blade.php
+++ resources/views/layoute/main.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perkiraan Cuaca Kecamatan</title>
+    <title>Pedia</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
